a26e23055146899bfb68f848ae8a8f46b7737c7c
Chris Bentivenga <chris@chrisbenti.com>
2021-03-28 16:11:36 -0700

    render loging

--- render.js
+++ render.js
@@ -4,18 +4,23 @@ const runJob = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   try {
+    console.log("waiting for page to load");
     await page.goto(`http://localhost:3000`, {
       waitUntil: "networkidle2",
     });
-
+    console.log("printing");
     await page.pdf({
       path: `output/index.pdf`,
       format: "Letter",
       printBackground: true,
     });
-  } catch {
+    console.log("printed");
+  } catch (e) {
+    console.error("Exception thrown", e.stack);
   } finally {
+    console.log("closing");
     await browser.close();
+    console.log("done");
   }
 };
 
