47b89d8e01bee193d0b23e9f068362e4b7046046
Ethan Goh <ethan@ethangoh.com>
2023-09-12 21:46:53 -0700

    copy updates

--- src/sections/jobExperience.tsx
+++ src/sections/jobExperience.tsx
@@ -6,39 +6,63 @@ const JobExperienceDetails = () => (
     <>
         <Row>
             <RowContent>
-                <RowHeader>Sourcebite</RowHeader>
-                <RowSubHeader>CTO</RowSubHeader>
+                <RowHeader>Chad Inc.</RowHeader>
+                <RowSubHeader>Cofounder / CTO</RowSubHeader>
                 <RowBody>
                     <PrettyUL>
                         <PrettyLI>
-                            Chief technological officer of Sourcebite (www.sourcebite.com), a B2B service that connects
-                            supply and demand of the food industry in Singapore. Sold to Uglyfood Pte Ltd.
+                            Spearheaded technical leadership and the development of Chad Inc., an AI-powered Shopify
+                            widget that transforms order management for customers and optimized merchant operations.
+                        </PrettyLI>
+                        <PrettyLI>
+                            Demonstrated exceptional leadership by achieving a 5-digit monthly recurring revenue and
+                            successfully scaling Chad to support 12 high-impact merchants.
+                        </PrettyLI>
+                        <PrettyLI>
+                            Led the entire technical lifecycle, from concept to launch, while overseeing a dynamic team
+                            of 5 engineers. Actively designed, coordinated and contributed to the code base, evolving it
+                            from functional prototype into a robust, scalable production application.
+                        </PrettyLI>
+                        <PrettyLI>
+                            Assumed the pivotal role of sole people manager for the technical team, including
+                            establishing a comprehensive performance management process, upleveling the engineering
+                            team. Additionally, was responsible for all engineering hiring, from managing external
+                            recruiting agencies, designing an interview process custom to Chad’s needs, and bringing in
+                            the individuals to the team.
                         </PrettyLI>
                     </PrettyUL>
                 </RowBody>
             </RowContent>
-            <RowPostFix>Jan 2020 to Nov 2020</RowPostFix>
+            <RowPostFix>June 2022 to Aug 2023</RowPostFix>
         </Row>
         <Row>
             <RowContent>
                 <RowHeader>Stripe</RowHeader>
-                <RowSubHeader>Software Engineer - Banking Platform</RowSubHeader>
+                <RowSubHeader>Staff Software Engineer - Cash Reporting</RowSubHeader>
                 <RowBody>
                     <PrettyUL>
                         <PrettyLI>
-                            Technical Lead for the Cash Reporting team - grew the team from 1 to 6 in 2 years.
-                            Re-architected a cash reporting pipeline for a 10x speed improvement to process bank
-                            transaction/balance reports (SWIFT/BAI) used for Stripe's cash reconciliation. Partnered
-                            closely with our downstream dependencies such as Accounting, Liquidity, and Treasury
-                            business functions, who sustain Stripe's daily business operations. Our team is the domain
-                            expert for standard bank reporting formats (e.g SWIFT, BAI2), and provides engineering
-                            support to teams building banking integrations (including bank transfer rails like
-                            ACH/SEPA).
+                            Led a team of 6 engineers responsible for processing business-critical bank reporting
+                            (SWIFT/BAI), pivotal to Stripe's accounting reconciliation and bank transfer products.
+                            Managed seamless communication and collaboration across stakeholders, including internal
+                            teams (Accounting, Treasury, Banking Operations) and external banking partners.
+                        </PrettyLI>
+                        <PrettyLI>
+                            Orchestrated the architectural overhaul and migration of report processing, shifting from a
+                            legacy, single-threaded pipeline to a distributed event driven system. This transformation
+                            substantially enhanced service reliability and reduced processing time by hours. As a
+                            result, Stripe efficiently met escalating regulatory demands and expanded support for new
+                            product categories and geographies.
+                        </PrettyLI>
+                        <PrettyLI>
+                            Spearheaded a cross-team initiative to develop a platform empowering global engineering
+                            teams to implement locale-specific bank transfer rails (ACH/SEPA) for the core Stripe
+                            Payments product.
                         </PrettyLI>
                     </PrettyUL>
                 </RowBody>
             </RowContent>
-            <RowPostFix>July 2019 to Present</RowPostFix>
+            <RowPostFix>July 2019 to May 2022</RowPostFix>
         </Row>
         <Row>
             <RowContent>
@@ -47,21 +71,20 @@ const JobExperienceDetails = () => (
                 <RowBody>
                     <PrettyUL>
                         <PrettyLI>
-                            Technical lead for the Open Platform Fleet Management team, architecting and creating
-                            tooling for the Level 5 (Autonomous Vehicle Division) and various Open Platform partners.
-                            Provisioned and integrated autonomous vehicles to service rides on the Lyft platform, which
-                            has served the worlds largest public autonomous pilot in Las Vegas.
+                            As the technical lead for the Open Platform Fleet Management team, I architected and created
+                            tooling to support the development of autonomous vehicles for both Level 5 and various Open
+                            Platform partners. This tooling provisioned and integrated autonomous vehicles into the Lyft
+                            platform, enabling rides for the world's largest public autonomous pilot in Las Vegas.
                         </PrettyLI>
                         <PrettyLI>
-                            Technical lead for the Driver Education team, where we built data driven systems to
-                            encourage drivers to provide a quality passenger experience. Architected Lyft's rating and
-                            feedback service that aggregates users' feedback across the app to uphold the quality of the
-                            product.
+                            Led the development of data-driven systems for the Driver Education team, aiming to enhance
+                            driver performance and passenger satisfaction. This included the design and implementation
+                            of Lyft's rating and feedback service to ensure consistently high-quality service.
                         </PrettyLI>
                         <PrettyLI>
-                            Designed and implemented the venues experience on the Airports &amp; Venues team, which
-                            serves 10% of all Lyft rides. Created tools for customer service agents to customize the
-                            pickup experience for sticky areas in their local markets.
+                            Designed and implemented the venues experience for the Airports & Venues team, which served
+                            10% of all Lyft rides. Additionally, I delivered tools that empowered customer support teams
+                            to customize pickup experiences in challenging local market areas.
                         </PrettyLI>
                     </PrettyUL>
                 </RowBody>
@@ -71,40 +94,21 @@ const JobExperienceDetails = () => (
         <Row>
             <RowContent>
                 <RowHeader>Microsoft</RowHeader>
-                <RowSubHeader>Software Engineer II - Azure Media Services</RowSubHeader>
+                <RowSubHeader>Software Engineer</RowSubHeader>
                 <RowBody>
                     <PrettyUL>
                         <PrettyLI>
-                            Architected a distributed media storage solution using Azure Storage that reduced COGS by
-                            30%. Developed backend services for Office 365 Video and defined the Media Services REST
-                            APIs.
+                            Designed Azure Storage solution, cutting Azure Media Services COGS by 30%. Developed backend
+                            and REST APIs for Office 365 Video.
                         </PrettyLI>
                         <PrettyLI>
-                            Improved the deployment speed of Media Services by 7x by rearchitecting the engineering
-                            pipeline.
+                            Revamped Lync Online deployment for seamless upgrades. Redesigned monitoring for Lync Online
+                            Server. Enhanced Lync Server reliability and performance through load redistribution.
                         </PrettyLI>
                     </PrettyUL>
                 </RowBody>
             </RowContent>
             <RowPostFix>Sep 2012 - Dec 2015</RowPostFix>
-        </Row>
-        <Row>
-            <RowContent>
-                <RowSubHeader>Software Engineer - Lync Server</RowSubHeader>
-                <RowBody>
-                    <PrettyUL>
-                        <PrettyLI>
-                            Improved Lync Server's reliability and performance by algorithmically partitioning and
-                            redistributing user load evenly across hardware.
-                        </PrettyLI>
-                        <PrettyLI>
-                            Designed and achieved seamless upgrades of the Lync Online by revamping the deployment
-                            infra. Redesigned Lync's monitoring system to oversee the health of the Lync Online Server.
-                        </PrettyLI>
-                    </PrettyUL>
-                </RowBody>
-            </RowContent>
-            <RowPostFix></RowPostFix>
         </Row>
     </>
 );
